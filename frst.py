eecab909d8c9f7c4ac38f2a9b8c6627b8ebb57e8
msolomentsev <michael.solomentsev@gmail.com>
2018-05-16 16:37:36 -0400

    increase in thresholding generality

--- frst.py
+++ frst.py
@@ -36,76 +36,57 @@ def frst(img, radii, alpha, beta, stdFactor, mode='BOTH'):
   dark = (mode == 'DARK' or mode == 'BOTH')
   bright = (mode == 'BRIGHT' or mode == 'BOTH')
 
-  output = np.zeros(img.shape, np.uint8)
-
-  rows, cols = img.shape
   workingDims = tuple((e + 2*radii) for e in img.shape)
 
+  #Set up output and M and O working matrices
+  output = np.zeros(img.shape, np.uint8)
   O_n = np.zeros(workingDims, np.int16)
   M_n = np.zeros(workingDims, np.int16)
 
+  #Calculate gradients
   gx = gradx(img)
   gy = grady(img)
 
+  #Find gradient vector magnitude
   gnorms = np.sqrt( np.add( np.multiply(gx, gx) , np.multiply(gy, gy) ) )
 
-  gthresh = 181*beta #181 is max possible gnorm value, from sqrt(2*128^2)
+  #Use beta to set threshold - speeds up transform significantly
+  gthresh = np.amax(gnorms)*beta
 
+  #Find x/y distance to affected pixels
   gpx = np.multiply(np.divide(gx, gnorms, out=np.zeros(gx.shape), where=gnorms!=0), radii).round().astype(int);
   gpy = np.multiply(np.divide(gy, gnorms, out=np.zeros(gy.shape), where=gnorms!=0), radii).round().astype(int);
 
+  #Iterate over all pixels (w/ gradient above threshold)
   for coords, gnorm in np.ndenumerate(gnorms):
     if gnorm > gthresh:
       i, j = coords
+      #Positively affected pixel
       if bright:
         ppve = (i+gpx[i,j], j+gpy[i,j])
         O_n[ppve] += 1
         M_n[ppve] += gnorm
+      #Negatively affected pixel
       if dark:
         pnve = (i-gpx[i,j], j-gpy[i,j])
         O_n[pnve] -= 1
         M_n[pnve] -= gnorm
 
+  #Abs and normalize O matrix
   O_n = np.abs(O_n)
   O_n = O_n / float(np.amax(O_n))
 
+  #Normalize M matrix
   M_max = float(np.amax(np.abs(M_n)))
   M_n = M_n / M_max
 
+  #Elementwise multiplication
   F_n = np.multiply(np.power(O_n, alpha), M_n)
 
+  #Gaussian blur
   kSize = int( np.ceil( radii / 2 ) )
   kSize = kSize + 1 if kSize % 2 == 0 else kSize
 
   S = cv2.GaussianBlur(F_n, (kSize, kSize), int( radii * stdFactor ))
 
   return S
-
-#returns coordinates of local maxima in the image
-#image: image to be analyzed, grayscale
-#size: how large is the filter - i.e. size of region of pixels that local maxima are found in
-#distance: minimum distance between coordinates
-def maxima(image, size, distance):
-    image_max = ndi.maximum_filter(im, size=size, mode='reflect')
-
-    # Comparison between image_max and im to find the coordinates of local maxima
-    coordinates = peak_local_max(im, min_distance=distance)
-
-    return coordinates
-
-#crops out sections of the image of size: [2*radius+1, 2*radius+1] at specified coordinates
-#Saves each section in the grape_training folder
-#image: image to be cropped
-#coordinates: coordinates to crop at
-#radius: size of crops
-#prefix: string that will be the start of every file name
-def bulk_crop(image, coordinates,radius, prefix):
-    [ymax, xmax, blah] = image.shape
-    image = np.abs(image)
-    i = 0
-    for x,y in coordinates:
-        if (x-radius>0 and y-radius>0 and x+radius<xmax and y+radius<ymax):
-            img_crop = image[y-radius:y+radius, x-radius:x+radius];
-            name = 'grape_training/'+prefix+str(i)+'.png'
-            cv2.imwrite(name, img_crop)
-            i+=1
