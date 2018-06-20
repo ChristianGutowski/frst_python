b5fd2f99e65ecce10d37c1a7e6cf4386a4e6ed6c
msolomentsev <michael.solomentsev@gmail.com>
2018-06-19 22:48:36 -0400

    pypi prep

---
+++ README.md
@@ -0,0 +1,6 @@
+# Fast Radial Symmetry Transform (FRST)
+
+Implementation of fast radial symmetry transform in pure Python using OpenCV and numpy.
+
+Based on:
+Loy, G., & Zelinsky, A. (2002). A fast radial symmetry transform for detecting points of interest. Computer Vision, ECCV 2002.
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="frst",
+    version="1.0.0",
+    author="Michael Solomentsev; Ryan O'Hern",
+    author_email="mys29@cornell.com",
+    description="Fast Radial Symmetry Transform",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohern/frst_python",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU3 License",
+        "Operating System :: OS Independent",
+    ),
+)
